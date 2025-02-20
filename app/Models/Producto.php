e173639485c0e33767fba6dd1065103646d64482
jadepulga02@gmail.com <jadepulga02@gmail.com>
2025-02-20 17:41:57 -0500

    cambio simple inicio archiv txt

--- app/Models/Producto.php
+++ app/Models/Producto.php
@@ -9,4 +9,5 @@ class Producto extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductoFactory> */
     use HasFactory;
+    protected $fillable = ['nombre', 'descripcion', 'precio', 'stock'];
 }
