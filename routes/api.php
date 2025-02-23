7c74822fdd1e3688a52948e182f0fbdf4ac41d61
jadepulga02@gmail.com <jadepulga02@gmail.com>
2025-02-23 16:37:18 -0500

    Actualizacion de version con arreglo del codigo por error

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use zApp\Http\Controllers\Api\V1\ProductoController;
+use App\Http\Controllers\Api\V1\ProductoController;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
