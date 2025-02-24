b462dabef4904283187f043b32209b14cc7d510b
jadepulga02@gmail.com <jadepulga02@gmail.com>
2025-02-23 22:31:06 -0500

    con carpeta api

--- routes/api.php
+++ routes/api.php
@@ -4,6 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\V1\ProductoController;
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource("v1/productos", App\Http\Controllers\Api\V1\ProductoController::class);
